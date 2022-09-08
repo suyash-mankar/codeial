77bc088307335639ae6507fb402eba13dec35fb5
suyash-mankar <suyashmankar@gmail.com>
2022-09-08 11:01:40 +0530

    set up user schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {
+    //crated at and updated at what time/date
+    timestamps: true
+});
+
+
+const User = monoogse.model('User', userSchema);
+
+module.exports = User;
