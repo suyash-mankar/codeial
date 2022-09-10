0149365541947a2536ba2b706f83aea3b01d95c3
suyash-mankar <suyashmankar@gmail.com>
2022-09-10 17:36:58 +0530

    add signout feature

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -66,3 +66,14 @@ module.exports.createSession = function(req,res){
     return res.redirect('/');    
 }
 
+
+
+module.exports.destroySession = function(req, res){
+    //logout function given to req by passport js
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+    });
+    return res.redirect('/');
+}
