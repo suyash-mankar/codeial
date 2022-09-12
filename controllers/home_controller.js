d2baaaab093d41b9060bd8c1099c34c3e1bff6cc
suyash-mankar <suyashmankar@gmail.com>
2022-09-12 16:32:38 +0530

    display comment and related user in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -18,21 +18,26 @@ module.exports.home = function(req, res){
     
 
     // Populate the user of each post
-    Post.find({}).populate(['user', 'comments']).exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        // further populate
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){
             console.log('error in finding posts from db');
             return;
         }
 
-
-        
-
-
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
         });
-    })
+    });
 
 }
 
