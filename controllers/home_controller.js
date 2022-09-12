ad7492643599c5f2c9a2371e74f98c93708116d9
suyash-mankar <suyashmankar@gmail.com>
2022-09-12 22:22:10 +0530

    add user proile links

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
 
@@ -33,10 +34,16 @@ module.exports.home = function(req, res){
             return;
         }
 
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts,
+                all_users: users
+            });
         });
+
+
+        
     });
 
 }
