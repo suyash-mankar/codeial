01c5c3faeed4e39689be832477ed6d55f47d74a5
suyash-mankar <suyashmankar@gmail.com>
2022-09-16 16:42:40 +0530

    set up directory structure for API and render API

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,11 @@ router.get('/', homeController.home);
 
 // whenever any request comes with users, it requires/goes-to users routes
 router.use('/users', require('./users'));
-
-
-// for any further routes access from here
-// router.use('/routerName', require('./routerFile'));  
-
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 
+
+router.use('/api', require('./api'));
 
 
 
