5d2c34f4cc4e4d17aedfcf63e26f171e2fdb6da1
suyash-mankar <suyashmankar@gmail.com>
2022-09-26 19:45:21 +0530

    typo

--- index.js
+++ index.js
@@ -114,5 +114,5 @@ const server = app.listen(port, function(err){
 // setup the chat server to be used with socket.io
 // const chatServer = require('http').createServer(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(server);
-// chatServer.listen(5000);
 console.log('chat server is listening on port 8000');
+
